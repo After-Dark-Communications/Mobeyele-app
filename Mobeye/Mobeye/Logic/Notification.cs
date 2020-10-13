104d834249b63c83a8075d510073d6d589cf1a1f
Lauralvh1995 <lauralvh@hotmail.com>
2020-10-13 11:44:10 +0200

    fixed tabs

--- Mobeye/Mobeye/Logic/Notification.cs
+++ Mobeye/Mobeye/Logic/Notification.cs
@@ -2,16 +2,16 @@ using System;
 
 public class Notification
 {
-    private int ID { get; set; }
-    private string Message { get; set; }
+	private int ID { get; set; }
+	private string Message { get; set; }
 
-    public void PushToDevice(NotificationModel notification)
-    {
-        throw new NotImplementedException();
-    }
+	public void PushToDevice(NotificationModel notification)
+	{
+		throw new NotImplementedException();
+	}
 
-    public bool ConfirmNotification(NotificationModel notification)
-    {
-        throw new NotImplementedException();
-    }
+	public bool ConfirmNotification(NotificationModel notification)
+	{
+		throw new NotImplementedException();
+	}
 }
--- Mobeye/Mobeye/Logic/User.cs
+++ Mobeye/Mobeye/Logic/User.cs
@@ -2,12 +2,16 @@ using System;
 
 public class User
 {
-   public bool LogIn(UserModel user)
-   {
-       throw new NotImplementedException();
-   }
-   public bool LogOut()
-   {
-       throw new NotImplementedException();
-   }
+    public bool LogIn(UserModel user)
+    {
+        throw new NotImplementedException();
+    }
+    public bool LogOut()
+    {
+        throw new NotImplementedException();
+    }
+    public void RedirectTo(int perms)
+    {
+        throw new NotImpementedException();
+    }
 }
