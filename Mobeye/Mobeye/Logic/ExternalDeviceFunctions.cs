93dcd24427bf1352f9648c12977a5cafc874bfb3
Lauralvh1995 <lauralvh@hotmail.com>
2020-10-13 11:38:31 +0200

    Update ExternalDeviceFunctions.cs

--- Mobeye/Mobeye/Logic/ExternalDeviceFunctions.cs
+++ Mobeye/Mobeye/Logic/ExternalDeviceFunctions.cs
@@ -2,7 +2,7 @@ using System;
 
 public class ExternalDeviceFunctions
 {
-    public ExecuteCommand(DeviceModel device)
+    public void ExecuteCommand(DeviceModel device)
     {
         throw new NotImplementedException();
     }
