bf310015ce1694378b24aaa0ce5fdf01739c1395
Lauralvh1995 <lauralvh@hotmail.com>
2020-10-13 11:40:26 +0200

    Update Notification.cs

--- Mobeye/Mobeye/Logic/Notification.cs
+++ Mobeye/Mobeye/Logic/Notification.cs
@@ -5,12 +5,12 @@ public class Notification
     private int ID { get; set; }
     private string Message { get; set; }
 
-    public void PushToDevice(Notification notification)
+    public void PushToDevice(NotificationModel notification)
     {
         throw new NotImplementedException();
     }
 
-    public bool ConfirmNotification(Notification notification)
+    public bool ConfirmNotification(NotificationModel notification)
     {
         throw new NotImplementedException();
     }
