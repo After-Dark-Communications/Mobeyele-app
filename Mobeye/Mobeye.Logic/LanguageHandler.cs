0f3ea5c3a13a688c5b48ed99f5e4255777be2c82
Lauralvh1995 <lauralvh@hotmail.com>
2020-10-13 14:47:21 +0200

    Create LanguageHandler.cs
    Added the LanguageHandler to Logic

---
+++ Mobeye/Mobeye.Logic/LanguageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobeye.Logic
+{
+    public class LanguageHandler
+    {
+        private Language Language { get; set; }
+
+        public void ChangeLanguage(Language lang)
+        {
+            Language = lang;
+        }
+    }
+
+    public enum Language
+    {
+        English,
+        Dutch,
+        German
+    }
+}
