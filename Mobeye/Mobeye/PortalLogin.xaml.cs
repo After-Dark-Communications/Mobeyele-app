960c9a7b2d9e368654a73a60129abe16cb126d6a
Steef Weijdeven <413985@student.fontys.nl>
2020-10-13 13:37:32 +0200

    added basic login
    artificial

--- Mobeye/Mobeye/PortalLogin.xaml.cs
+++ Mobeye/Mobeye/PortalLogin.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
@@ -13,12 +15,48 @@ namespace Mobeye
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PortalLogin : ContentPage
     {
+        private string username, password;
+
         public PortalLogin()
         {
             InitializeComponent();
             On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
         }
-        async void OpenURL(object sender, EventArgs e) => await Browser.OpenAsync("https://www.technetgroup.nl", BrowserLaunchMode.External);
+        async void OpenURL(object sender, EventArgs e) => await openTestSite();
+
+        async void LoginWithPortalAccount(object sender, EventArgs e)
+        {
+            setUsernameAndPassword(Username.Text, Password.Text);
+            //pass through to Logic to check if matching username and password
+            if (quicktest(username, password))
+            {
+               await openTestSite();
+            }
+            else
+            {
+                await DisplayAlert("Couldn't Log In.", "The username/password were incorrect", "OK");
+            }
+        }
+
+        private void setUsernameAndPassword(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        private bool quicktest(string username, string password)
+        {
+            if (username == "john.doe@mail.com" && password == "123456")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private Task openTestSite()
+        {
+           return Browser.OpenAsync("https://www.technetgroup.nl", BrowserLaunchMode.External);
+        }
     }
 
 }
