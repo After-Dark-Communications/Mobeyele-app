5bad3d0488fc4c95093405e2582cc8f742d824a4
nino verheijen <ninoverheijen@hotmail.com>
2020-10-13 13:48:08 +0200

    Added APIHelper class Added HttpClient nuget to the API layer

---
+++ Mobeye/Mobeye.API/APIHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Mobeye.API
+{
+    public static class APIHelper
+    {
+        public static HttpClient APIClient { get; set; }
+        public static void InitializeClient()
+        {
+            APIClient = new HttpClient();
+            APIClient.DefaultRequestHeaders.Accept.Clear();
+            APIClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ Mobeye/Mobeye.API/APILoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobeye.API
+{
+    public class APILoginModel
+    {
+        public int AccountId { get; set; }
+        public string AccountPhoneNumber { get; set; }
+    }
+}
