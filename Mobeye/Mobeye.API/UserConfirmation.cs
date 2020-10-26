18e550fc020a645cf92c3f44f1afc651f087d085
nino verheijen <ninoverheijen@hotmail.com>
2020-10-26 11:31:47 +0100

    Added async Task to PortalOwnerConfirmationRequest Created PortalOwnerConfirmationRequest

--- Mobeye/Mobeye.API/UserConfirmation.cs
+++ Mobeye/Mobeye.API/UserConfirmation.cs
@@ -1,27 +1,44 @@
-﻿using System;
+﻿using Mobeye.Dependency;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mobeye.API
 {
     public class UserConfirmation
-    {
-        
-        public string GottenCode { get; set; }
-        public string GottenTel { get; set; }
-
-        public bool SendTelConfirmRequest(string tel)
+    {       
+        public async Task<bool> GetTelConfirmRequest(string tel)
         {
+            throw new NotImplementedException();
+        }
+        public async Task<bool> GetCodeConfirmRequest(string code)
+        {
+            using (HttpResponseMessage response = await APIHelper.API.GetAsync(""))
+            {
+                if(response.IsSuccessStatusCode)
+                {
+                    string receivedcode = await response.Content.ReadAsStringAsync();
+                    if(code == receivedcode)
+                    {
+                        return true;
+                    }
+                }
+            }
             return false;
         }
-        public async Task<bool> SendCodeConfirmRequest(string code)
+        public async Task<UserModel> PortalOwnerConfirmationRequest(UserModel user)
         {
-            return false;
-        }
-        public bool PortalOwnerConfirmationRequest(string username, string password)
-        {
-            return false;
+            using(HttpResponseMessage response = await APIHelper.API.GetAsync(""))
+            {
+                if(response.IsSuccessStatusCode)
+                {
+                    UserModel receivedUser = await response.Content.ReadAsAsync<UserModel>();
+                    return receivedUser;
+                }
+            }
+            return null;
         }
     }
 }
