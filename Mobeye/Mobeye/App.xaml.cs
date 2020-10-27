a0192767c1532e2548a5dbf7f5ab8808da9e3f82
Steef Weijdeven <413985@student.fontys.nl>
2020-10-27 11:26:12 +0100

    made a few changes to ios visuals

--- Mobeye/Mobeye/App.xaml.cs
+++ Mobeye/Mobeye/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 
 namespace Mobeye
@@ -10,9 +11,11 @@ namespace Mobeye
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
-
-
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromHex("#C5D93E"),
+                BarTextColor = Color.White,
+            };
         }
 
         protected override void OnStart()
