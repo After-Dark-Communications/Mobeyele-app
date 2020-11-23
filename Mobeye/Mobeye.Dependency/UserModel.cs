0911ae5c519ea561cff0fe91f3202652dfa0f208
Lauralvh1995 <lauralvh@hotmail.com>
2020-11-23 14:27:45 +0100

    Fixed UserModel

--- Mobeye/Mobeye.Dependency/UserModel.cs
+++ Mobeye/Mobeye.Dependency/UserModel.cs
@@ -9,21 +9,20 @@ namespace Mobeye.Dependency
         public string SmsKey { get; set; }
         public string PrivateKey { get; set; }
         public string Name { get; set; }
-        public string Imei { get; set; }
         public string Phonenumber { get; set; }
         public List<DeviceModel> ExternalDevices { get; set; }
         public int PermissionLevel { get; set; }
-        public UserModel()
-        {
-
-        }
-        public UserModel(string smskey, string privatekey, string name, string phonenumber,int permissionlevel )
+        public UserModel(string smskey, string privatekey, string name, string phonenumber, int permissionlevel)
         {
             this.SmsKey = smskey;
             this.PrivateKey = privatekey;
             this.Name = name;
             this.Phonenumber = phonenumber;
             this.PermissionLevel = permissionlevel;
+        }
+        public UserModel()
+        {
+
         }
     }
 }
