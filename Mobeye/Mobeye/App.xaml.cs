47e44875e890408731465b7e3768e749d057bcb0
michiel.vissers@student.fontys.nl <Rdp6PWWKNE>
2021-01-11 14:20:28 +0100

    first setup

--- Mobeye/Mobeye/App.xaml.cs
+++ Mobeye/Mobeye/App.xaml.cs
@@ -3,6 +3,9 @@ using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 using Mobeye.API;
+using Com.OneSignal;
+using Com.OneSignal.Abstractions;
+using System.Collections.Generic;
 
 namespace Mobeye
 {
@@ -13,6 +16,16 @@ namespace Mobeye
             InitializeComponent();
             ApiHelper.InitaliazeClient(Device.RuntimePlatform);
             MainPage = new NavigationPage(new MainPage());
+
+            OneSignal.Current.StartInit("YOUR_ONESIGNAL_APP_ID")
+            .Settings(new Dictionary<string, bool>() {
+            { IOSSettings.kOSSettingsKeyAutoPrompt, false },
+             { IOSSettings.kOSSettingsKeyInAppLaunchURL, false } })
+            .InFocusDisplaying(OSInFocusDisplayOption.Notification)
+            .EndInit();
+
+            // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+            OneSignal.Current.RegisterForPushNotifications();
         }
 
         protected override void OnStart()
