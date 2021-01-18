ce9f7ca4af7d200836f7423816887855f3d928ae
Naresh <n.rai@student.fontys.nl>
2021-01-18 16:00:57 +0100

    nothing

--- Mobeye/MobEye.Dependency/DeviceModel.cs
+++ Mobeye/MobEye.Dependency/DeviceModel.cs
@@ -5,9 +5,9 @@
         public string DeviceId { get; set; }
         public string Devicename { get; set; }
         public string CommandText { get; set; }
-        public string Command { get; set; }    
+        public string Command { get; set; }
 
-        public DeviceModel ( string deviceId, string deviceName, string commandText, string command)
+        public DeviceModel(string deviceId, string deviceName, string commandText, string command)
         {
             this.DeviceId = deviceId;
             this.Devicename = deviceName;
