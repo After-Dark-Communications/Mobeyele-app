d796e22465b28dfb9491dc410ceac309b58e7365
nino verheijen <ninoverheijen@hotmail.com>
2021-01-18 15:33:16 +0100

    Changed Notification Model to string

--- Mobeye/MobEye.Dependency/NotificationModel.cs
+++ Mobeye/MobEye.Dependency/NotificationModel.cs
@@ -2,11 +2,11 @@
 {
     public class NotificationModel
     {
-        public string PhoneId { get; set; }
-        public int UniqueMessageId { get; set; }
-        public string Response { get; set; }
-        public string PrivateKey { get; set; }
-
+        public string Authorization { get; set; }
+        public string Devicename { get; set; }
+        public string Devicelocation { get; set; }
+        public string Alarmtext { get; set; }
+        public string Value { get; set; }
         //Property names are based on the POST example from Mobeye. Do not change these!
     }
 }
