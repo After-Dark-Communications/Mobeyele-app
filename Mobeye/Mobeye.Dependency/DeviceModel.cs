152117c8d56cf4aee67755e48e841ea88d423175
Naresh <n.rai@student.fontys.nl>
2021-01-18 16:00:31 +0100

    fix

--- Mobeye/Mobeye.Dependency/DeviceModel.cs
+++ Mobeye/Mobeye.Dependency/DeviceModel.cs
@@ -2,9 +2,17 @@
 {
     public class DeviceModel
     {
-        public string Auth { get; set; }
+        public string DeviceId { get; set; }
         public string Devicename { get; set; }
-        public string Devicelocation { get; set; }
+        public string CommandText { get; set; }
         public string Command { get; set; }
+
+        public DeviceModel(string deviceId, string deviceName, string commandText, string command)
+        {
+            this.DeviceId = deviceId;
+            this.Devicename = deviceName;
+            this.CommandText = commandText;
+            this.Command = command;
+        }
     }
 }
